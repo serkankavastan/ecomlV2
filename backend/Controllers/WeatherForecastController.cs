16b1f60883770bfe2b37ab51a227b1a1b4777834
Serkan Kavastan <serkankavastan@gmail.com>
2021-03-08 21:49:42 +0300

    yeni dosyalar

--- backend/Controllers/WeatherForecastController.cs
+++ backend/Controllers/WeatherForecastController.cs
@@ -31,7 +31,8 @@ namespace backend.Controllers
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
+                Summary = Summaries[rng.Next(Summaries.Length)],
+                Writer = "Serkan"
             })
             .ToArray();
         }
